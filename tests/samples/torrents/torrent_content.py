35120ffe2b2815c9cf32de9765a8cfda07d0abdc
Bigfootpp <97189022+Bigfootpp@users.noreply.github.com>
2026-08-31 04:48:17 +0200

    test(torrent): add sample

---
+++ tests/samples/torrents/torrent_content.py
@@ -0,0 +1,72 @@
+# DO NOT IMPORT THIS FILE
+
+from saga.models.torrent import ResolvedTorrent, TorrentFileEntry
+
+# ./call_of_the_night.torrent
+ResolvedTorrent(
+    title="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC) | (Yofukashi no Uta Season 2)",
+    info_hash="f3584460e77f6f7d5ea3327e0cc1b4c4f51db1d2",
+    magnet="magnet:?xt=urn:btih:f3584460e77f6f7d5ea3327e0cc1b4c4f51db1d2&amp;dn=%5BShroud%5D+Call+of+the+Night+Season+2+(1080p+BD+REMUX+AVC+FLAC)+%7C+(Yofukashi+no+Uta+Season+2)&amp;tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&amp;tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&amp;tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&amp;tr=udp%3A%2F%2Fvito-tracker.space%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Fvito-tracker.duckdns.org%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.srv00.com%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.qu.ax%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.corpscorp.online%3A80%2Fannounce",
+    files=[
+        TorrentFileEntry(
+            file_idx=0,
+            file_name="[Shroud] Call of the Night Season 2 - S02E01 - That Time`s Not for Us. (1080p BD REMUX AVC FLAC) [8403377E].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E01 - That Time`s Not for Us. (1080p BD REMUX AVC FLAC) [8403377E].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=1,
+            file_name="[Shroud] Call of the Night Season 2 - S02E02 - I`ve Been Looking for You. (1080p BD REMUX AVC FLAC) [10CF1A7E].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E02 - I`ve Been Looking for You. (1080p BD REMUX AVC FLAC) [10CF1A7E].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=2,
+            file_name="[Shroud] Call of the Night Season 2 - S02E03 - There Aren`t Any Ghosts in a Haunter House. (1080p BD REMUX AVC FLAC) [3C79AFD3].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E03 - There Aren`t Any Ghosts in a Haunter House. (1080p BD REMUX AVC FLAC) [3C79AFD3].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=3,
+            file_name="[Shroud] Call of the Night Season 2 - S02E04 - Do You Want to Be Able to Run (1080p BD REMUX AVC FLAC) [FAED1B2A].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E04 - Do You Want to Be Able to Run (1080p BD REMUX AVC FLAC) [FAED1B2A].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=4,
+            file_name="[Shroud] Call of the Night Season 2 - S02E05 - The Few Years I Spent with You... (1080p BD REMUX AVC FLAC) [02946E1D].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E05 - The Few Years I Spent with You... (1080p BD REMUX AVC FLAC) [02946E1D].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=5,
+            file_name="[Shroud] Call of the Night Season 2 - S02E06 - I`m Not Asking About the Quality! (1080p BD REMUX AVC FLAC) [554BD3F2].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E06 - I`m Not Asking About the Quality! (1080p BD REMUX AVC FLAC) [554BD3F2].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=6,
+            file_name="[Shroud] Call of the Night Season 2 - S02E07 - You`re Too Stupid for That. (1080p BD REMUX AVC FLAC) [FA9036F8].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E07 - You`re Too Stupid for That. (1080p BD REMUX AVC FLAC) [FA9036F8].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=7,
+            file_name="[Shroud] Call of the Night Season 2 - S02E08 - You`re the First, Senpai. (1080p BD REMUX AVC FLAC) [369693FA].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E08 - You`re the First, Senpai. (1080p BD REMUX AVC FLAC) [369693FA].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=8,
+            file_name="[Shroud] Call of the Night Season 2 - S02E09 - Yamori-kun, What Do You Want to Do (1080p BD REMUX AVC FLAC) [7FD97C6A].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E09 - Yamori-kun, What Do You Want to Do (1080p BD REMUX AVC FLAC) [7FD97C6A].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=9,
+            file_name="[Shroud] Call of the Night Season 2 - S02E10 - I Wanted It to Be You. (1080p BD REMUX AVC FLAC) [6BA93489].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E10 - I Wanted It to Be You. (1080p BD REMUX AVC FLAC) [6BA93489].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=10,
+            file_name="[Shroud] Call of the Night Season 2 - S02E11 - Mwah... (1080p BD REMUX AVC FLAC) [46D95C57].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E11 - Mwah... (1080p BD REMUX AVC FLAC) [46D95C57].mkv",
+        ),
+        TorrentFileEntry(
+            file_idx=11,
+            file_name="[Shroud] Call of the Night Season 2 - S02E12 - Call of the Night (1080p BD REMUX AVC FLAC) [CA1C957C].mkv",
+            path="[Shroud] Call of the Night Season 2 (1080p BD REMUX AVC FLAC)/[Shroud] Call of the Night Season 2 - S02E12 - Call of the Night (1080p BD REMUX AVC FLAC) [CA1C957C].mkv",
+        ),
+    ],
+)
