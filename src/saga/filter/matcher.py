1e7dc99aa9cd2e002d5136fe99d06cb57b5e4587
Bigfootpp <97189022+Bigfootpp@users.noreply.github.com>
2026-09-02 20:22:59 +0200

    feat(torrents): implement matcher to find file_idx

---
+++ src/saga/filter/matcher.py
@@ -0,0 +1,43 @@
+from RTN import parse
+
+from saga.models.query import SeriesQuery
+from saga.models.torrent import MediaFile, ResolvedTorrent, TorrentFileEntry
+
+
+class NoMatchError(Exception):
+    pass
+
+
+def _filter_files(files: list[TorrentFileEntry], season: int) -> list[TorrentFileEntry]:
+    result: list[TorrentFileEntry] = []
+    for i, file in enumerate(files):
+        parsed_file_name_data = parse(file.file_name)
+        if not parsed_file_name_data.seasons:
+            result.append(file)
+            continue
+        if (
+            len(parsed_file_name_data.seasons) == 1
+            and season in parsed_file_name_data.seasons
+        ):
+            result.append(file)
+
+    return result
+
+
+def find_file_idx(torrent: ResolvedTorrent, query: SeriesQuery) -> MediaFile:
+    files = _filter_files(torrent.files, query.season)
+    for file in files:
+        parsed_file_name = parse(file.file_name)
+        if (
+            parsed_file_name.episodes
+            and len(parsed_file_name.episodes) == 1
+            and query.episode in parsed_file_name.episodes
+        ):
+            return MediaFile(
+                file_name=file.file_name,
+                info_hash=torrent.info_hash,
+                magnet=torrent.magnet,
+                file_idx=file.file_idx,
+            )
+
+    raise NoMatchError("No matched file found")
