c13cad196cb5b426cd5d2d5b4555b3c36ec1c519
Bigfootpp <97189022+Bigfootpp@users.noreply.github.com>
2026-09-12 18:38:47 +0200

    feat(api): add manifest route

---
+++ src/saga/api/manifest.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+from saga.models.manifest import ManifestResponse
+
+router = APIRouter()
+
+
+@router.get("/manifest.json")
+@router.get("/{config}/manifest.json")
+async def get_manifest() -> ManifestResponse:
+    return ManifestResponse(
+        id="community.bigfootpp.saga",
+        icon="https://i.imgur.com/tVjqEJP.png",
+        name="Saga",
+        version="0.1.0",
+        description="Elevate your Stremio experience with seamless access to Jackett torrent links, effortlessly "
+        "fetching torrents for your selected movies within the Stremio interface.",
+        resources=["stream"],
+        types=["movie", "series"],
+        catalogs=[],
+    )
