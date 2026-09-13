8eeb7bde8b8d9396d9d632d9fb761ec3ed8cff17
Bigfootpp <97189022+Bigfootpp@users.noreply.github.com>
2026-09-13 18:58:30 +0200

    forget to save

--- src/saga/models/config.py
+++ src/saga/models/config.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class Config(BaseModel):
+class UserConfig(BaseModel):
     model_config = ConfigDict(populate_by_name=True, extra="allow")
 
     preferred_dubs: list[str] = Field(..., alias="preferredDubs")
